95515efaf94e05ceb2fbc267eb4170625ab15e68
Reza Tavasoli <jfk998@gmail.com>
2019-08-28 00:21:19 +0430

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: null,
+    );
+  }
+}
