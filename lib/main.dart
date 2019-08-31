18896c329808642dbd00bf607acc7ac853e16830
Reza Tavasoli <jfk998@gmail.com>
2019-08-31 22:56:12 +0430

    Update app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hangman/Screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,8 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: null,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF3e38b0),
+        textTheme: Theme.of(context).textTheme.apply(fontFamily: 'MiriamLibre'),
+      ),
+      home: HomeScreen(),
     );
   }
 }
