b142250329ecc91e19a9d12a1169e37512513e91
Reza Tavasoli <jfk998@gmail.com>
2019-09-07 00:57:23 +0430

    Add constants utilities

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kActionButtonTextStyle = TextStyle(
+  fontSize: 35.0,
+  color: Colors.white,
+);
+const kActionButtonColor = Color(0xFF6b48ff);
+const kActionButtonHighlightColor = Color(0xFF099a97);
+
+const kWordButtonColor = Color(0xFF099a97);
+const kWordButtonTextStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
