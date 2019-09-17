c8cbb6bdb089b09c725cb3e7ae145625cfb930c9
Reza Tavasoli <jfk998@gmail.com>
2019-09-17 16:39:41 +0430

    Add new constants

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 const kActionButtonTextStyle = TextStyle(
   fontSize: 35.0,
@@ -8,5 +9,37 @@ const kActionButtonColor = Color(0xFF6b48ff);
 const kActionButtonHighlightColor = Color(0xFF099a97);
 
 const kWordButtonColor = Color(0xFF099a97);
+
 const kWordButtonTextStyle =
     TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+
+var kAlertStyle = AlertStyle(
+  animationType: AnimationType.grow,
+  isCloseButton: false,
+  isOverlayTapDismiss: false,
+  descStyle: TextStyle(fontWeight: FontWeight.bold),
+  animationDuration: Duration(milliseconds: 400),
+  backgroundColor: Color(0xFF2C1E68),
+  alertBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10.0),
+  ),
+  titleStyle: TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 25.0,
+  ),
+);
+
+const kDialogButtonTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const kWordTextStyle = TextStyle(
+    fontSize: 60,
+    color: Colors.white,
+    fontFamily: 'FiraMono',
+    letterSpacing: 8);
+
+const kDialogButtonColor = Color(0xFF6b48ff);
