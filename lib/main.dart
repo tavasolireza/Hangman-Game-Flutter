72962de14716febf0c9fa380182798b300e4a1f8
Reza Tavasoli <jfk998@gmail.com>
2019-09-27 16:26:53 +0330

    Hide status bar and debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hangman/screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Color(0xFF3e38b0),
         textTheme: Theme.of(context).textTheme.apply(fontFamily: 'AmaticSC'),
