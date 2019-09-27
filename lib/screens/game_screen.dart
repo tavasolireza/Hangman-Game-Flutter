b525ff8ff192e3068371d7b832fd8b25ef3a6878
Reza Tavasoli <jfk998@gmail.com>
2019-09-27 16:29:47 +0330

    Add hangman states and failing option

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -18,19 +18,18 @@ class _GameScreenState extends State<GameScreen> {
   String hiddenWord;
   List<String> wordList = [];
   List<bool> buttonStatus;
+  int hangState = 0;
 
   void initWords() {
-    print('i entered here');
+    hangState = 0;
     buttonStatus = List.generate(26, (index) {
       return true;
     });
     wordList = [];
     word = hangmanWords.getWord();
     if (word.length != 0) {
-      print(word.length);
       hiddenWord = hangmanWords.getHiddenWord(word.length);
     } else {
-      print('fuck');
       hiddenWord = '';
       Alert(
               context: context,
@@ -45,18 +44,50 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   void wordPress(int index) {
+    bool check = false;
     setState(() {
       for (int i = 0; i < wordList.length; i++) {
         if (wordList[i] == englishAlphabet.alphabet[index]) {
+          check = true;
           wordList[i] = '';
           hiddenWord = hiddenWord.replaceFirst(RegExp('_'), word[i], i);
         }
       }
+      if (!check) {
+        hangState += 1;
+      }
+      if (hangState == 6) {
+        Alert(
+          context: context,
+          style: kFailedAlertStyle,
+          type: AlertType.error,
+          title: word,
+          desc: "You failed!",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Next Word",
+                style: kDialogButtonTextStyle,
+              ),
+              onPressed: () {
+                setState(() {
+                  Navigator.pop(context);
+                  initWords();
+                });
+              },
+              width: 120,
+              color: kDialogButtonColor,
+              height: 45,
+            )
+          ],
+        ).show();
+      }
+
       buttonStatus[index] = false;
       if (hiddenWord == word) {
         Alert(
           context: context,
-          style: kAlertStyle,
+          style: kSuccessAlertStyle,
           type: AlertType.success,
           title: word,
           desc: "You guessed it right!",
@@ -91,30 +122,52 @@ class _GameScreenState extends State<GameScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print(wordList);
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: <Widget>[
             Expanded(
-              flex: 3,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 20.0),
-                alignment: Alignment.center,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text(
-                    hiddenWord,
-                    style: kWordTextStyle,
-                  ),
-                ),
-              ),
-            ),
+                flex: 3,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(top: 10.0),
+                        alignment: Alignment.bottomCenter,
+                        width: 160,
+                        child: FittedBox(
+                          child: Image.asset(
+                            'images/$hangState.png',
+                            height: 1001,
+                            width: 991,
+                          ),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 30.0),
+                        alignment: Alignment.center,
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text(
+                            hiddenWord,
+                            style: kWordTextStyle,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
             Expanded(
               flex: 2,
               child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 18.0),
-                padding: EdgeInsets.symmetric(vertical: 15),
+                //width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                padding: EdgeInsets.only(bottom: 10),
                 child: GridView.count(
                   primary: false,
                   crossAxisCount: 6,
