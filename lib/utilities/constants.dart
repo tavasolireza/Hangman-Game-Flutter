1396e07f1281efa27d231fdf9c33101bb2b29e75
Reza Tavasoli <jfk998@gmail.com>
2019-11-04 23:21:17 +0330

    Update constants

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,15 +4,19 @@ import 'package:rflutter_alert/rflutter_alert.dart';
 const kActionButtonTextStyle = TextStyle(
   fontSize: 30.0,
   color: Colors.white,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 1.0,
 );
-const kActionButtonColor = Color(0xFF6b48ff);
-const kActionButtonHighlightColor = Color(0xFF099a97);
+const kActionButtonColor = Color(0xFF3e64ff);
+const kActionButtonHighlightColor = Color(0xFF3e64ff);
 
-const kWordButtonColor = Color(0xFF099a97);
+const kWordButtonColor = Color(0xFF3e64ff);
+const kTooltipColor = Color(0xFF3e64ff);
 
-const kWordButtonTextStyle =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+const kWordButtonTextStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontSize: 29.5,
+);
 
 var kSuccessAlertStyle = AlertStyle(
   animationType: AnimationType.grow,
@@ -21,7 +25,8 @@ var kSuccessAlertStyle = AlertStyle(
   descStyle: TextStyle(
     color: Colors.white,
     fontWeight: FontWeight.bold,
-    fontSize: 25.0,
+    fontSize: 27.0,
+    letterSpacing: 2.0,
   ),
   animationDuration: Duration(milliseconds: 500),
   backgroundColor: Color(0xFF2C1E68),
@@ -29,11 +34,35 @@ var kSuccessAlertStyle = AlertStyle(
     borderRadius: BorderRadius.circular(10.0),
   ),
   titleStyle: TextStyle(
-    color: Color(0xFF099a97),
+    color: Color(0xFF00e676),
     fontWeight: FontWeight.bold,
-    fontSize: 25.0,
+    fontSize: 28.0,
+    letterSpacing: 2.0,
   ),
 );
+var kExitAlertStyle = AlertStyle(
+  animationType: AnimationType.grow,
+  isCloseButton: false,
+  isOverlayTapDismiss: false,
+  descStyle: TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 27.0,
+    letterSpacing: 2.0,
+  ),
+  animationDuration: Duration(milliseconds: 500),
+  backgroundColor: Color(0xFF2C1E68),
+  alertBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10.0),
+  ),
+  titleStyle: TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 27.0,
+    letterSpacing: 2.0,
+  ),
+);
+
 var kFailedAlertStyle = AlertStyle(
   animationType: AnimationType.grow,
   isCloseButton: false,
@@ -41,9 +70,10 @@ var kFailedAlertStyle = AlertStyle(
   descStyle: TextStyle(
     color: Colors.white,
     fontWeight: FontWeight.bold,
-    fontSize: 25.0,
+    fontSize: 27.0,
+    letterSpacing: 2.0,
   ),
-  animationDuration: Duration(milliseconds: 500),
+  animationDuration: Duration(milliseconds: 450),
   backgroundColor: Color(0xFF2C1E68),
   alertBorder: RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(10.0),
@@ -51,20 +81,25 @@ var kFailedAlertStyle = AlertStyle(
   titleStyle: TextStyle(
     color: Colors.red,
     fontWeight: FontWeight.bold,
-    fontSize: 25.0,
+    fontSize: 28.0,
+    letterSpacing: 2.0,
   ),
 );
 
 const kDialogButtonTextStyle = TextStyle(
   color: Colors.white,
-  fontSize: 21,
+  fontSize: 25,
   fontWeight: FontWeight.bold,
+  letterSpacing: 1.0,
 );
 
 const kWordTextStyle = TextStyle(
-    fontSize: 60,
+    fontSize: 57,
     color: Colors.white,
     fontFamily: 'FiraMono',
     letterSpacing: 8);
 
-const kDialogButtonColor = Color(0xFF6b48ff);
+const kDialogButtonColor = Color(0xFF3e64ff);
+
+const kWordCounterTextStyle =
+    TextStyle(fontSize: 29.5, color: Colors.white, fontWeight: FontWeight.w900);
