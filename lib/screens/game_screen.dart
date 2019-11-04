94b642cedd8313a03a4f7b56d7784855500f0b6b
Reza Tavasoli <jfk998@gmail.com>
2019-11-04 23:27:57 +0330

    Add score counter and exit and help buttons

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,49 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hangman/screens/home_screen.dart';
 import 'package:flutter_hangman/utilities/alphabet.dart';
 import 'package:flutter_hangman/components/word_button.dart';
 import 'package:flutter_hangman/utilities/constants.dart';
 import 'package:flutter_hangman/utilities/hangman_words.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'dart:math';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class GameScreen extends StatefulWidget {
+  GameScreen({@required this.hangmanObject});
+
+  final HangmanWords hangmanObject;
+
   @override
   _GameScreenState createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
-  HangmanWords hangmanWords = HangmanWords();
   Alphabet englishAlphabet = Alphabet();
   String word;
   String hiddenWord;
   List<String> wordList = [];
+  List<int> hintLetters = [];
   List<bool> buttonStatus;
+  bool hintStatus;
   int hangState = 0;
+  int wordCount = 0;
+  bool finishedGame = false;
+  bool resetGame = false;
+
+  Widget createButton(index) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 3.5, vertical: 6.0),
+      child: Center(
+        child: WordButton(
+          buttonTitle: englishAlphabet.alphabet[index].toUpperCase(),
+          onPress: buttonStatus[index] ? () => wordPress(index) : null,
+        ),
+      ),
+    );
+  }
 
   void initWords() {
+    finishedGame = false;
+    resetGame = false;
+    hintStatus = true;
     hangState = 0;
     buttonStatus = List.generate(26, (index) {
       return true;
     });
     wordList = [];
-    word = hangmanWords.getWord();
+    hintLetters = [];
+    word = widget.hangmanObject.getWord();
     if (word.length != 0) {
-      hiddenWord = hangmanWords.getHiddenWord(word.length);
+      hiddenWord = widget.hangmanObject.getHiddenWord(word.length);
     } else {
-      hiddenWord = '';
-      Alert(
-              context: context,
-              title: "Game is Finished",
-              desc: "You've guessed all of the words right!")
-          .show();
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        ModalRoute.withName('homePage'),
+      );
     }
 
     for (int i = 0; i < word.length; i++) {
       wordList.add(word[i]);
+      hintLetters.add(i);
     }
   }
 
   void wordPress(int index) {
+    if (finishedGame) {
+      setState(() {
+        resetGame = true;
+      });
+      return;
+    }
+
     bool check = false;
     setState(() {
       for (int i = 0; i < wordList.length; i++) {
@@ -53,10 +87,17 @@ class _GameScreenState extends State<GameScreen> {
           hiddenWord = hiddenWord.replaceFirst(RegExp('_'), word[i], i);
         }
       }
+      for (int i = 0; i < wordList.length; i++) {
+        if (wordList[i] == '') {
+          hintLetters.remove(i);
+        }
+      }
       if (!check) {
         hangState += 1;
       }
+
       if (hangState == 6) {
+        finishedGame = true;
         Alert(
           context: context,
           style: kFailedAlertStyle,
@@ -65,6 +106,7 @@ class _GameScreenState extends State<GameScreen> {
           desc: "You failed!",
           buttons: [
             DialogButton(
+              radius: BorderRadius.circular(10),
               child: Text(
                 "Next Word",
                 style: kDialogButtonTextStyle,
@@ -75,9 +117,9 @@ class _GameScreenState extends State<GameScreen> {
                   initWords();
                 });
               },
-              width: 120,
+              width: 118,
               color: kDialogButtonColor,
-              height: 45,
+              height: 50,
             )
           ],
         ).show();
@@ -85,6 +127,7 @@ class _GameScreenState extends State<GameScreen> {
 
       buttonStatus[index] = false;
       if (hiddenWord == word) {
+        finishedGame = true;
         Alert(
           context: context,
           style: kSuccessAlertStyle,
@@ -93,19 +136,21 @@ class _GameScreenState extends State<GameScreen> {
           desc: "You guessed it right!",
           buttons: [
             DialogButton(
+              radius: BorderRadius.circular(10),
               child: Text(
                 "Next Word",
                 style: kDialogButtonTextStyle,
               ),
               onPressed: () {
                 setState(() {
+                  wordCount += 1;
                   Navigator.pop(context);
                   initWords();
                 });
               },
-              width: 120,
+              width: 118,
               color: kDialogButtonColor,
-              height: 45,
+              height: 50,
             )
           ],
         ).show();
@@ -117,77 +162,250 @@ class _GameScreenState extends State<GameScreen> {
   void initState() {
     super.initState();
     initWords();
-    print(word);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                flex: 3,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.only(top: 10.0),
-                        alignment: Alignment.bottomCenter,
-                        width: 160,
-                        child: FittedBox(
-                          child: Image.asset(
-                            'images/$hangState.png',
-                            height: 1001,
-                            width: 991,
-                          ),
-                          fit: BoxFit.contain,
+    if (resetGame) {
+      print('i came here');
+      setState(() {
+        initWords();
+      });
+    }
+    print(wordList);
+    return WillPopScope(
+      onWillPop: () {
+        return Future(() => false);
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(6.0, 8.0, 6.0, 45.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(top: 0.6),
+                              child: IconButton(
+                                tooltip: 'Home',
+                                highlightColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                iconSize: 30,
+                                icon: Icon(MdiIcons.home),
+                                onPressed: () {
+                                  Alert(
+                                    context: context,
+                                    style: kExitAlertStyle,
+                                    title: "Exit current game?",
+                                    buttons: [
+                                      DialogButton(
+                                        radius: BorderRadius.circular(10),
+                                        child: Text(
+                                          "Yes",
+                                          style: kDialogButtonTextStyle,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomeScreen()),
+                                            ModalRoute.withName('homePage'),
+                                          );
+                                        },
+                                        width: 87,
+                                        color: Colors.green,
+                                        height: 48,
+                                      ),
+                                      DialogButton(
+                                        radius: BorderRadius.circular(10),
+                                        child: Text(
+                                          "No",
+                                          style: kDialogButtonTextStyle,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            Navigator.pop(context);
+                                          });
+                                        },
+                                        width: 87,
+                                        color: Colors.red,
+                                        height: 48,
+                                      ),
+                                    ],
+                                  ).show();
+                                },
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                '$wordCount',
+                                style: kWordCounterTextStyle,
+                              ),
+                            ),
+                            Container(
+                              child: IconButton(
+                                tooltip: 'Hint',
+                                iconSize: 30,
+                                icon: Icon(MdiIcons.lightbulb),
+                                highlightColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                onPressed: hintStatus
+                                    ? () {
+                                        int rand = Random()
+                                            .nextInt(hintLetters.length);
+                                        wordPress(englishAlphabet.alphabet
+                                            .indexOf(
+                                                wordList[hintLetters[rand]]));
+                                        hintStatus = false;
+                                      }
+                                    : null,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.symmetric(horizontal: 30.0),
-                        alignment: Alignment.center,
-                        child: FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Text(
-                            hiddenWord,
-                            style: kWordTextStyle,
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          alignment: Alignment.bottomCenter,
+                          child: FittedBox(
+                            child: Image.asset(
+                              'images/$hangState.png',
+                              height: 1001,
+                              width: 991,
+                              gaplessPlayback: true,
+                            ),
+                            fit: BoxFit.contain,
                           ),
                         ),
                       ),
-                    ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30.0),
+                          alignment: Alignment.center,
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Text(
+                              hiddenWord,
+                              style: kWordTextStyle,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                padding: EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 8.0),
+                child: Table(
+                  defaultVerticalAlignment: TableCellVerticalAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  //columnWidths: {1: FlexColumnWidth(10)},
+                  children: [
+                    TableRow(children: [
+                      TableCell(
+                        child: createButton(0),
+                      ),
+                      TableCell(
+                        child: createButton(1),
+                      ),
+                      TableCell(
+                        child: createButton(2),
+                      ),
+                      TableCell(
+                        child: createButton(3),
+                      ),
+                      TableCell(
+                        child: createButton(4),
+                      ),
+                      TableCell(
+                        child: createButton(5),
+                      ),
+                      TableCell(
+                        child: createButton(6),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      TableCell(
+                        child: createButton(7),
+                      ),
+                      TableCell(
+                        child: createButton(8),
+                      ),
+                      TableCell(
+                        child: createButton(9),
+                      ),
+                      TableCell(
+                        child: createButton(10),
+                      ),
+                      TableCell(
+                        child: createButton(11),
+                      ),
+                      TableCell(
+                        child: createButton(12),
+                      ),
+                      TableCell(
+                        child: createButton(13),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      TableCell(
+                        child: createButton(14),
+                      ),
+                      TableCell(
+                        child: createButton(15),
+                      ),
+                      TableCell(
+                        child: createButton(16),
+                      ),
+                      TableCell(
+                        child: createButton(17),
+                      ),
+                      TableCell(
+                        child: createButton(18),
+                      ),
+                      TableCell(
+                        child: createButton(19),
+                      ),
+                      TableCell(
+                        child: createButton(20),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      TableCell(
+                        child: createButton(21),
+                      ),
+                      TableCell(
+                        child: createButton(22),
+                      ),
+                      TableCell(
+                        child: createButton(23),
+                      ),
+                      TableCell(
+                        child: createButton(24),
+                      ),
+                      TableCell(
+                        child: createButton(25),
+                      ),
+                      TableCell(
+                        child: Text(''),
+                      ),
+                      TableCell(
+                        child: Text(''),
+                      ),
+                    ]),
                   ],
-                )),
-            Expanded(
-              flex: 2,
-              child: Container(
-                //width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 10.0),
-                padding: EdgeInsets.only(bottom: 10),
-                child: GridView.count(
-                  primary: false,
-                  crossAxisCount: 6,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 5,
-                  children: List.generate(
-                    26,
-                    (index) {
-                      return Center(
-                          child: WordButton(
-                        buttonTitle: englishAlphabet.alphabet[index],
-                        onPress:
-                            buttonStatus[index] ? () => wordPress(index) : null,
-                      ));
-                    },
-                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
