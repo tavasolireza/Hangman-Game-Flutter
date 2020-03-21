ca09481b283593d7e7da9d6f1ce80f3f3f879e71
Reza Tavasoli <jfk998@gmail.com>
2020-03-21 10:26:09 +0430

    Add Score class

---
+++ lib/utilities/user_scores.dart
@@ -0,0 +1,22 @@
+class Score {
+  final int id;
+  final String scoreDate;
+  final int userScore;
+
+  Score({this.id, this.scoreDate, this.userScore});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'scoreDate': scoreDate,
+      'userScore': userScore,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog{id: $id, scoreDate: $scoreDate, userScore: $userScore}';
+  }
+}
