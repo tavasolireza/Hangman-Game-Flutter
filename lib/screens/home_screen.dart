de4b0f0fbd33f9d90274a7055e9a63c838d2938a
Reza Tavasoli <jfk998@gmail.com>
2020-03-21 15:31:00 +0430

    Fix button widths

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -49,46 +49,42 @@ class _HomeScreenState extends State<HomeScreen> {
             height: height * 0.055,
           ),
           Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: 150,
-                  height: 59,
-                  child: ActionButton(
-                    buttonTitle: 'New Game',
-                    onPress: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GameScreen(
-                            hangmanObject: widget.hangmanWords,
+            child: IntrinsicWidth(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+//                    width: 155,
+                    height: 62,
+                    child: ActionButton(
+                      buttonTitle: 'Start',
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GameScreen(
+                              hangmanObject: widget.hangmanWords,
+                            ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Container(
-                  width: 150,
-                  height: 59,
-                  child: ActionButton(
-                    buttonTitle: 'High Scores',
-                    onPress: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GameScreen(
-                            hangmanObject: widget.hangmanWords,
-                          ),
-                        ),
-                      );
-                    },
+                  SizedBox(
+                    height: 15.0,
                   ),
-                ),
-              ],
+                  Container(
+//                    width: 155,
+                    height: 65,
+                    child: ActionButton(
+                      buttonTitle: 'High Scores',
+                      onPress: () {
+                        Navigator.pushNamed(context, 'scorePage');
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
