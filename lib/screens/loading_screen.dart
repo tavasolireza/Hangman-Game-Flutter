c87bb5644a7e2a12facad4a0d517be9ca2c5d915
Reza Tavasoli <jfk998@gmail.com>
2020-03-21 21:37:28 +0430

    Add loading screen before showing scores

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hangman/screens/score_screen.dart';
+import 'package:flutter_hangman/utilities/score_db.dart' as score_database;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    queryScores();
+  }
+
+  void queryScores() async {
+    final database = score_database.openDB();
+    var queryResult = await score_database.scores(database);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return ScoreScreen(
+            query: queryResult,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
